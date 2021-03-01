33a15d133897cc96d17c5eb5438969680a569eb8
scoot <niko665a@zbc.dk>
2021-03-01 08:53:26 +0100

    Add project files.

---
+++ Jedi mind tricks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_mind_tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1
+            Dictionary<string, int> people = new Dictionary<string, int>
+            {
+                // Opgave 2
+                { "Nikolai", 18 },
+                { "Han", 88 }
+            };
+            people["Alex"] = 18;
+
+            // ??
+            Console.WriteLine(people["Nikolai"]);
+
+            people.Remove("Han");
+
+
+            foreach(var p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
